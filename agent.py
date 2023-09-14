181b089dc2ddded4c26409d5deb5039f0a777ac4
YintianZhan <yintianzhan2021@u.northwestern.edu>
2023-09-13 23:46:33 -0400

    add map

--- agent.py
+++ agent.py
@@ -2,7 +2,17 @@ import numpy as np
 import pandas as pd
 
 class Agent:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name = 'Alex', age = 20, location = (0,0)) -> None:
+        self.name = name
+        self.age = age
+        self.hp = 100 - max((20 - self.age), 0) * 5 - max((self.age - 50), 0) * 5
+        self.location = location
+    
+    def describe(self):
+        print('The agent {} is {} years old, with {:.2f} hp, and is at {}'.format(self.name, self.age, self.hp, self.location))
 
-print('hello')
+
+
+
+a = Agent()
+a.describe()
---
+++ map.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class Map:
+    def __init__(self, width, length) -> None:
+        self.width = width
+        self.length = length
+
+    def 
