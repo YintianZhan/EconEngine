e9300b4774587b3fe21b35df84ceb6bdd34fa5f5
YintianZhan <yintianzhan2021@u.northwestern.edu>
2023-09-16 23:06:03 -0400

    added reset and act

--- agent.py
+++ agent.py
@@ -11,6 +11,7 @@ import torch
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
+TOTAL_HRS = 24
 
 class Agent:
     def __init__(self, name = 'Alex', age = 20, location = (0,0), efficiency = 1) -> None:
@@ -21,6 +22,7 @@ class Agent:
         self.efficiency = efficiency
         self.product = {resource_name:0 for resource_name in resource_dict.keys()}
         self.utilities = 0
+        self.hrs_spent = 0
 
         self.n_games = 0
         self.epsilon = 0 # randomness
@@ -30,7 +32,7 @@ class Agent:
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
     def describe(self, map = None):
-        print('The agent {} is {} years old, with {:.2f} hp, and is at {}'.format(self.name, self.age, self.hp, self.location))
+        print('The agent {} is {} years old, with {:.2f} hp {:.2f} utilities, and is at {}'.format(self.name, self.age, self.hp, self.utilities, self.location))
         print('{} has:'.format(self.name))
         if len(self.product) > 0:
             for resource, units in self.product.items():
@@ -41,6 +43,29 @@ class Agent:
         if map is not None:
             print('{} is at:'.format(self.name))
             map.print_map([self.location])
+    
+    def act(self, model_action):
+        if model_action[0] == 1:
+            reward = self.move_location((0,1))
+        elif model_action[1] == 1:
+            reward = self.move_location((1,0))
+        elif model_action[2] == 1:
+            reward = self.produce_local()
+        elif model_action[3] == 1:
+            reward = self.cook()
+        elif model_action[4] == 1:
+            reward = self.consume()
+        self.hrs_spent += 1
+        game_over = (self.hp == 0) or (self.hrs_spent == TOTAL_HRS)
+
+        return reward, game_over, self.utilities
+
+    def reset(self):
+        self.hp = 100 - max((20 - self.age), 0) * 5 - max((self.age - 50), 0) * 5
+        self.location = (0, 0)
+        self.product = {resource_name:0 for resource_name in resource_dict.keys()}
+        self.utilities = 0
+        self.hrs_spent = 0
 
     def produce_resource(self, resource, map = None):
         if not resource_dict[resource].recipe.requires_location or (map != None and resource == map.map[self.location[0]][self.location[1]]):
@@ -54,13 +79,14 @@ class Agent:
         else:
             print('{} could not produce {}'.format(self.name, resource))
 
+        return 0
+
     def produce_local(self, map):
-        self.produce_resource(map.map[self.location[0]][self.location[1]], map)
+        return self.produce_resource(map.map[self.location[0]][self.location[1]], map)
 
     def cook(self):
-        self.produce_resource('cooked fish')
-        
-
+        return self.produce_resource('cooked fish')
+    
     def trade(self, inflows, inflow_units, outflows, outflow_units):
         try:
             assert len(inflows) == len(inflow_units)
@@ -81,6 +107,7 @@ class Agent:
                 print(inflows[i], inflow_units[i])
         except Exception as e:
             print('{} could not trade'.format(self.name))
+        return 0
 
     def consume(self, resource):
         if self.product[resource] >= 1:
@@ -90,23 +117,12 @@ class Agent:
             print('{} ate {}'.format(self.name, resource))
         else:
             print('{} could not eat {}'.format(self.name, resource))
+        return resource_dict[resource].consumption_utility
 
     def product_decay(self):
         if len(self.product) > 0:
             for resource, units in self.product.items():
                 self.product[resource] *= units.decay
-
-    def act(self, model_action):
-        if model_action[0] == 1:
-            self.move_location((0,1))
-        elif model_action[1] == 1:
-            self.move_location((1,0))
-        elif model_action[2] == 1:
-            self.produce_local()
-        elif model_action[3] == 1:
-            self.cook()
-        elif model_action[4] == 1:
-            self.consume()
 
     def move_location(self, step, map):
         assert abs(sum(step)) == 1
@@ -115,6 +131,7 @@ class Agent:
         old_location = self.location
         self.location = (self.location[0] + step[0], self.location[1] + step[1])
         print('{} moved from {} to {}'.format(self.name, old_location, self.location))
+        return 0
 
     def get_state(self):
 
